3183648722c70840c3fa0b32ff1e581ef2de2569
Cho KwangEun <rhkddms1021@gmail.com>
2023-03-30 17:13:16 +0900

    [update] viewpager 어뎁터 주석 추가

--- ViewpagerTabLayoutPro/app/src/main/java/com/example/viewpagertablayoutpro/MainActivity.kt
+++ ViewpagerTabLayoutPro/app/src/main/java/com/example/viewpagertablayoutpro/MainActivity.kt
@@ -21,7 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        
+        //리사이클러뷰(어뎁터 설정)
         customAdapter = CustomAdapter(this)
         customAdapter.addListFragment(OneFragment())
         customAdapter.addListFragment(TwoFragment())
@@ -40,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 //        binding.tabLayout.addTab(tab1)
 //        binding.tabLayout.addTab(tab2)
 //        binding.tabLayout.addTab(tab3)
+        //탭레이아웃 뷰페이저 연동
         TabLayoutMediator(binding.tabLayout, binding.viewpager2){tab,position ->
             tab.setCustomView(tabCustomView(position))
         }.attach()
@@ -48,19 +50,22 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         //2. ActionBarDrawerToggle 버튼 적용
         toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.drawer_open, R.string.drawer_close)
-        //3. 업버튼 활성화
+        //3. 업버튼 활성화(BackPress 기능을 담당 이전화면으로 돌아가는 기능 액션바에 만들어주는 기능)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //4. 토글 sync
+        //4. 토글 sync(업버튼자리에 토글버튼을 동기화시킴)
         toggle.syncState()
-
+        
+        // 네비게이션뷰에있는 메뉴항목을 클릭했을때 이벤트 처리
         binding.navigationView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener{
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
                 when(item.itemId){
+                    R.id.item_age -> Toast.makeText(applicationContext, "age", Toast.LENGTH_SHORT).show()
+                    R.id.item_email -> Toast.makeText(applicationContext, "email", Toast.LENGTH_SHORT).show()
                     R.id.item_info -> Toast.makeText(applicationContext, "info", Toast.LENGTH_SHORT).show()
+                    R.id.item_user -> Toast.makeText(applicationContext, "user", Toast.LENGTH_SHORT).show()
                 }
                 return false
             }
-
         })
     }
 
