cb023ad37351a4411723f3c9ea16d188b86c6024
Cho KwangEun <rhkddms1021@gmail.com>
2023-04-04 17:39:06 +0900

    [create] IntentSevenSolutionProject 인텐트로 화면전환,객체전달 방법들

---
+++ IntentSevenSolutionPro/app/src/main/java/com/example/intentsevensolutionpro/LoginData.kt
@@ -0,0 +1,5 @@
+package com.example.intentsevensolutionpro
+
+import java.io.Serializable
+
+data class LoginData(val id: String, val pwd:String) : Serializable
---
+++ IntentSevenSolutionPro/app/src/main/java/com/example/intentsevensolutionpro/MainActivity2.kt
@@ -0,0 +1,48 @@
+package com.example.intentsevensolutionpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.intentsevensolutionpro.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //1단계 액티비티 전환
+        //2단계 데이터 받기(id, pwd)
+        if (intent.hasExtra("no")){
+            when(intent.getIntExtra("no", 0)){
+                //2단계 데이터 보내기
+                0 -> {
+                    if (intent.hasExtra("id").equals("") && intent.hasExtra("pwd").equals("")){
+                        Toast.makeText(this,"id와 pwd가 없습니다", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }else{
+                        val id = intent.getStringExtra("id")
+                        val pwd = intent.getStringExtra("pwd")
+                        Toast.makeText(this,"${id}와 ${pwd}가 있습니다", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                //3단계 데이터객체로 보내기(serializable)
+                3 -> {
+                    val loginData = intent.getSerializableExtra("loginData") as LoginData
+                    Toast.makeText(this,"${loginData.id}와 ${loginData.pwd} ", Toast.LENGTH_SHORT).show()
+                }
+                4 -> {
+                    val loginDataParcel = intent.getParcelableExtra<LoginDataParcel>("loginDataParcel")
+                    Toast.makeText(this,"$loginDataParcel?.id}와 ${loginDataParcel?.pwd} ", Toast.LENGTH_SHORT).show()
+                }
+                5 -> {
+                    val loginDataParcelList = intent.getParcelableArrayListExtra<LoginDataParcel>("loginDataParcelList")
+                    Toast.makeText(this,"${loginDataParcelList?.get(2)?.id}와 ${loginDataParcelList?.get(2)?.pwd} ", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
+    }//end of onCreate
+}
