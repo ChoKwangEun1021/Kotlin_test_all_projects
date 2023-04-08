89d678f22c6ed5c2b8b24cbf31119693164702a2
Cho KwangEun <rhkddms1021@gmail.com>
2023-04-08 22:38:59 +0900

    [create] create mp3 Test

---
+++ ContentResolver/app/src/main/java/com/example/contentresolver/Music.kt
@@ -0,0 +1,28 @@
+package com.example.contentresolver
+
+import android.net.Uri
+import android.provider.MediaStore
+
+class Music(id:String, title:String?, artist:String?, albumId:String?, duration:Long?) {
+    var id: String = "" //음원 자체의 ID
+    var title: String? = ""
+    var artist: String? = ""
+    var albumId: String? = "" //앨범이미지 ID
+    var duration: Long? = 0
+
+    init {
+        this.id = id
+        this.title = title
+        this.artist = artist
+        this.albumId = albumId
+        this.duration = duration
+    }
+
+    fun getMusicUri(): Uri {
+        return Uri.withAppendedPath(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
+    }
+
+    fun getAlbumUri(): Uri{
+        return Uri.parse("content://media/external/audio/albumrt/${albumId}")
+    }
+}
---
+++ ContentResolver/app/src/main/java/com/example/contentresolver/MusicAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.contentresolver
+
+import android.media.MediaPlayer
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contentresolver.databinding.ItemLayoutBinding
+import java.text.SimpleDateFormat
+
+class MusicAdapter : RecyclerView.Adapter<MusicAdapter.Holder>() {
+    val musicList = mutableListOf<Music>()
+    var mediaPlayer:MediaPlayer? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val itemLayoutBinding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return Holder(itemLayoutBinding)
+    }
+
+    override fun getItemCount() = musicList.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val music = musicList[position]
+        holder.setMusic(music)
+    }
+
+    inner class Holder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+        var musicUri: Uri? = null
+
+        init {
+            binding.root.setOnClickListener {
+                if (mediaPlayer != null){
+                    mediaPlayer?.release()
+                    mediaPlayer = null
+                }
+                mediaPlayer = MediaPlayer.create(binding.root.context, musicUri)
+                mediaPlayer?.start()
+            }
+        }
+
+        fun setMusic(music:Music){
+            musicUri = music.getMusicUri()
+            binding.imageAlbum.setImageURI(music.getAlbumUri())
+            binding.textArtist.text = music.artist
+            binding.textTitle.text = music.title
+            val sdf = SimpleDateFormat("mm:ss")
+            binding.textDuration.text = sdf.format(music.duration)
+        }
+    }
+}
+
