5069c60a69bd8ce91dc84e35c59281cbcee6e63f
Cho KwangEun <rhkddms1021@gmail.com>
2023-04-11 17:49:58 +0900

    [update] 임시 수정

--- MP3PlayerPro/app/src/main/java/com/example/mp3playerpro/PlayActivity.kt
+++ MP3PlayerPro/app/src/main/java/com/example/mp3playerpro/PlayActivity.kt
@@ -1,11 +1,13 @@
 package com.example.mp3playerpro
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.SeekBar
+import android.widget.Toast
 import com.example.mp3playerpro.databinding.ActivityPlayBinding
 import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
@@ -17,8 +19,8 @@ class PlayActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var musicData: MusicData
     var mp3PlayerJob: Job? = null
     var pauseFlag = false
-    var currentMusicIndex: Int = 0
-    var musicList: List<MusicData>? = null
+    var currentPosition:Int = 0
+    var musicList: MutableList<MusicData>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayBinding.inflate(layoutInflater)
@@ -26,10 +28,13 @@ class PlayActivity : AppCompatActivity(), View.OnClickListener {
 
         //전달해온 intent값을 가져온다
         musicData = intent.getSerializableExtra("musicData") as MusicData
+
+        musicList = intent.getSerializableExtra("musicList") as MutableList<MusicData>?
+
+        currentPosition = intent.getIntExtra("position", 0)
         // 재생 목록 가져오기
-        musicList = intent.getSerializableExtra("musicList") as? List<MusicData>
-        // 현재 재생 중인 음악의 인덱스 정보 초기화
-//        currentMusicIndex = musicList?.indexOf(musicData) ?: 0
+//        musicList = intent.getSerializableExtra("musicList") as? MutableList<MusicData>
+
 
         //화면에 바인딩 진행
         binding.albumTitle.text = musicData.title
@@ -120,58 +125,20 @@ class PlayActivity : AppCompatActivity(), View.OnClickListener {
                 binding.playButton.setImageResource(R.drawable.play_24)
             }
             R.id.previousButton -> {
-                // 현재 재생 중인 음악의 이전 음악을 찾아서 재생
-//                if (currentMusicIndex > 0) {
-//                    currentMusicIndex--
-//                } else {
-//                    currentMusicIndex = musicList!!.size - 1
-//                }
-//                val previousMusic = musicList!![currentMusicIndex]
-//                mediaPlayer?.stop()
-//                mediaPlayer?.release()
-//                mediaPlayer = MediaPlayer.create(this, previousMusic.getMusicUri())
-//                musicData = previousMusic
-//                binding.albumTitle.text = previousMusic.title
-//                binding.albumArtist.text = previousMusic.artist
-//                binding.totalDuration.text = SimpleDateFormat("mm:ss").format(previousMusic.duration)
-//                val bitmap = previousMusic.getAlbumBitmap(this, ALBUM_IMAGE_SIZE)
-//                if (bitmap != null) {
-//                    binding.albumImage.setImageBitmap(bitmap)
-//                } else {
-//                    binding.albumImage.setImageResource(R.drawable.music_video_24)
-//                }
-//                binding.seekBar.progress = 0
-//                binding.playDuration.text = "00:00"
-//                binding.seekBar.max = mediaPlayer!!.duration
-//                binding.playButton.setImageResource(R.drawable.pause_24)
-//                mediaPlayer?.start()
+                mediaPlayer?.stop()
+                mp3PlayerJob?.cancel()
+                binding.seekBar.progress = 0
+                binding.playDuration.text = "00:00"
+
+                val previousCurrentPosition = currentPosition - 1
+                if (previousCurrentPosition > 0) {
+                    currentPosition = musicList!!.size - 1
+                }else{
+                    currentPosition = previousCurrentPosition
+                }
             }
             R.id.nextButton -> {
-                // 다음 음악을 찾아서 재생
-//                if (currentMusicIndex < musicList!!.size - 1) {
-//                    currentMusicIndex++
-//                } else {
-//                    currentMusicIndex = 0
-//                }
-//                val nextMusic = musicList!![currentMusicIndex]
-//                mediaPlayer?.stop()
-//                mediaPlayer?.release()
-//                mediaPlayer = MediaPlayer.create(this, nextMusic.getMusicUri())
-//                musicData = nextMusic
-//                binding.albumTitle.text = nextMusic.title
-//                binding.albumArtist.text = nextMusic.artist
-//                binding.totalDuration.text = SimpleDateFormat("mm:ss").format(nextMusic.duration)
-//                val bitmap = nextMusic.getAlbumBitmap(this, ALBUM_IMAGE_SIZE)
-//                if (bitmap != null) {
-//                    binding.albumImage.setImageBitmap(bitmap)
-//                } else {
-//                    binding.albumImage.setImageResource(R.drawable.music_video_24)
-//                }
-//                binding.seekBar.progress = 0
-//                binding.playDuration.text = "00:00"
-//                binding.seekBar.max = mediaPlayer!!.duration
-//                binding.playButton.setImageResource(R.drawable.pause_24)
-//                mediaPlayer?.start()
+
             }
             R.id.likeButton -> {
 
